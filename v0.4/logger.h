4495add34bb3f4d056380a60288bcd8400e1b8a9
leafriel <79296816+leafriel@users.noreply.github.com>
2025-02-14 13:18:15 +0800

    Delete v0.4 directory

--- v0.4/logger.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "spdlog/spdlog.h"
-
-extern std::shared_ptr<spdlog::logger> GlobalLogger;
-
-void init_global_logger();
-void set_log_level(spdlog::level::level_enum log_level);
